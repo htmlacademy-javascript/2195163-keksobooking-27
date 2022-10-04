43db20d8e87cf03feb66e59162608f65afb80184
Artem <artemmendin777@gmail.com>
2022-10-04 17:27:59 +0500

    Добавил исправления

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const getRandomNumber = (min, max) => {
-    if (min < 0 || max < 0 || max === min) {
-        return NaN
-    }
-    if (min > max) {
-        [min, max] = [max, min]
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-getRandomNumber();
+const getRandomNumber = (min, max, count = 0) => {
+  if (min < 0 || max < 0 || max === min) {
+    return NaN;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return +(Math.random() * (max - min + 1) + min).toFixed(count);
+};
+getRandomNumber(1, 10, 3);

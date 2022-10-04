011df1ab4af35b954d0d26bcdc44932b911904ad
Keks <keks@htmlacademy.ru>
2022-10-04 15:28:52 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,7 +1,10 @@
-function randomNumber(min, max) {
-    if (min < 0 || max < 0) {
-        return NaN;
-    }
-}
-return Math.floor(Math.random() * (max - min)) + min
-randomNumber()
+const getRandomNumber = (min, max, count = 0) => {
+  if (min < 0 || max < 0 || max === min) {
+    return NaN;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return +(Math.random() * (max - min + 1) + min).toFixed(count);
+};
+getRandomNumber(1, 10, 3);

293908e5a23f7c2c619889e66a8a902133326d55
TimaRagnar <114170518+TimaRagnar@users.noreply.github.com>
2022-10-11 15:34:20 +0300

    Исаправления задания 4.15

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const TYPE_BUILDING = ["palace", "flat", "house", "bungalow", "hotel"];
-const TIME_CHECK = ["12:00", "13:00", "14:00"];
-const COMFORT = [
+const TYPES_OF_BUILDING = ["palace", "flat", "house", "bungalow", "hotel"];
+const CHECK_TIMES = ["12:00", "13:00", "14:00"];
+const FEATURES = [
   "wifi",
   "dishwasher",
   "parking",
@@ -8,20 +8,21 @@ const COMFORT = [
   "elevator",
   "conditioner",
 ];
-const PHOTOS_LIST = [
+const PHOTOS = [
   "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
   "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
   "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg",
 ];
-const TITLE_HOUSE = ["Квартира", "Дом", "Лофт", "Дача"];
-const DESCRIPTION_LIST = [
+const TITLES = ["Квартира", "Дом", "Лофт", "Дача"];
+const DESCRIPTIONS = [
   "Шикарноый вид из окна",
   "Отличный ремонт",
   "Есть вся мебель",
   "Есть вся техника для уютного проживания",
   "Хорошие соседи",
 ];
-const RANDOM_ROOM_COUNT = 10;
+
+const RANDOM_OFFER_COUNT = 10;
 
 const getRandomNumber = (min, max, count = 0) => {
   if (min < 0 || max < 0 || max === min) {
@@ -41,48 +42,42 @@ const getRandomArrayElement = (arr) => {
   return arr[i];
 };
 
-const getNewArray = (object) => {
-  const newArray = [];
-  const newArrayLength = getRandomNumber(1, object.length - 1);
+const getNewArray = (array) => array[getRandomNumber(0, array.length - 1)];
 
-  for (let i = 1; i <= newArrayLength; i++) {
-    const options = object.shift();
-    newArray.push(options);
+const createAvatar = () => {
+  let index = getRandomNumber(1, 10);
+  if (index < 10) {
+    index = `0${index}`;
   }
-
-  return newArray;
+  return `img/avatars/user/${index}.png`;
 };
 
-function n(n) {
-  {
-    return n > 9 ? "" + n : "0" + n;
-  }
-}
-const author = {
-  avatar: `img/avatars/user/${n(getRandomNumber(1, 10))}.png`,
-};
-const location1 = {
-  lat: getRandomNumber(35.65, 35.7, 5),
-  lng: getRandomNumber(139.7, 139.8, 5),
+const createOffer = () => {
+  const location = {
+    lat: getRandomNumber(35.65, 35.7, 5),
+    lng: getRandomNumber(139.7, 139.8, 5),
+  };
+  return {
+    author: {
+      avatar: createAvatar(),
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomNumber(10000, 100000),
+      type: getRandomArrayElement(TYPES_OF_BUILDING),
+      rooms: getRandomNumber(1, 4),
+      quests: getRandomNumber(1, 6),
+      checkin: getRandomArrayElement(CHECK_TIMES),
+      checkout: getRandomArrayElement(CHECK_TIMES),
+      features: FEATURES.slice(0, getRandomNumber(0, FEATURES.length)),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: Array.from({ length: getRandomNumber(1, 10) }, () =>
+        getNewArray(PHOTOS)
+      ),
+    },
+    location,
+  };
 };
 
-const createOffer = () => ({
-  author,
-  offer: {
-    title: getRandomArrayElement(TITLE_HOUSE),
-    address: `${location1.lat}, ${location1.lng}`,
-    price: getRandomNumber(10000, 100000),
-    type: getRandomArrayElement(TYPE_BUILDING),
-    rooms: getRandomNumber(1, 4),
-    quests: getRandomNumber(1, 6),
-    checkin: getRandomArrayElement(TIME_CHECK),
-    checkout: getRandomArrayElement(TIME_CHECK),
-    features: getNewArray(COMFORT),
-    description: getRandomArrayElement(DESCRIPTION_LIST),
-    photos: getNewArray(PHOTOS_LIST),
-  },
-  location1,
-});
-
-const randomRoom = Array.from({ length: RANDOM_ROOM_COUNT }, createOffer);
-console.log(randomRoom);
+const randomRoom = Array.from({ length: RANDOM_OFFER_COUNT }, createOffer);

135729c9ed3ac37b4944aa2b0bec322a19bc2dbe
TimaRagnar <114170518+TimaRagnar@users.noreply.github.com>
2022-11-01 21:36:28 +0300

    Домашняя работа 7.16(3)

--- js/render-card.js
+++ js/render-card.js
@@ -1,7 +1,5 @@
 const mapCanvas = document.querySelector('.map__canvas');
-const cardTemplate = document
-  .querySelector('#card')
-  .content.querySelector('.popup');
+const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 
 const TypeToName = {
   bungalow: 'Бунгало',
@@ -28,72 +26,79 @@ const renderAvatarBlock = (parent, cssClass, data) => {
   element.src = data;
 };
 
-const timeText = (offer) => {
-  if(offer.checkin && offer.checkout){
+const createTimeText = (offer) => {
+  if (offer.checkin && offer.checkout) {
     return `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  } else if(offer.checkin && !offer.checkout){
+  }
+  if (offer.checkin && !offer.checkout) {
     return `Заезд после ${offer.checkin}`;
-  } else if(!offer.checkin && offer.checkout){
+  }
+  if (!offer.checkin && offer.checkout) {
     return `Выезд до ${offer.checkout}`;
   }
-  return '';
+  return false;
 };
 
-const numberRoomGuest = (offer) => {
-  if(offer.rooms && offer.guests){
+const createCapacityText = (offer) => {
+  if (offer.rooms && offer.guests) {
     return `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  } else if(offer.rooms && !offer.guests){
+  }
+  if (offer.rooms && !offer.guests) {
     return `${offer.rooms} комнаты`;
-  } else if(!offer.rooms && offer.guests){
+  }
+  if (!offer.rooms && offer.guests) {
     return `Для ${offer.guests} гостей`;
   }
-  return '';
+  return false;
 };
 
 const renderPhotoBlock = (parent, cssClass, data) => {
   const element = parent.querySelector(cssClass);
-  if (!data) {
+  if (!data.length) {
     element.remove();
     return;
   }
-  element.src = data;
+  const photo = element.querySelector('img');
+  element.innerHTML = '';
+  data.forEach((item) => {
+    const cloningElement = photo.cloneNode(true);
+    cloningElement.src = item;
+    element.append(cloningElement);
+  });
 };
 
-const renderPhoto = (photos) => {
-  for (let i = 0; i < photos.length; i++) {
-    photos = photos[i];
-    return photos;
+const renderFeaturesBlock = (parent, cssClass, data) => {
+  const element = parent.querySelector(cssClass);
+  if (!data.length) {
+    element.remove();
+    return;
   }
+  const elementItem = document.createElement('li');
+  elementItem.classList.add('popup__feature');
+  element.innerHTML = '';
+  data.forEach((item) => {
+    const cloningElement = elementItem.cloneNode(true);
+    cloningElement.classList.add(`popup__feature--${item}`);
+    element.append(cloningElement);
+  });
 };
 
 const createCardTemplate = (data) => {
   const cardElement = cardTemplate.cloneNode(true);
   renderTextBlock(cardElement, '.popup__title', data.offer.title);
   renderTextBlock(cardElement, '.popup__text--address', data.offer.address);
-  renderTextBlock(
-    cardElement,
-    '.popup__text--price',
-    data.offer.price,
-    ' ₽/ночь'
-  );
+  renderTextBlock(cardElement, '.popup__text--price', data.offer.price, ' ₽/ночь');
   renderTextBlock(cardElement, '.popup__type', TypeToName[data.offer.type]);
   renderTextBlock(cardElement, '.popup__description', data.offer.description);
+  renderTextBlock(cardElement, '.popup__text--capacity', createCapacityText(data.offer));
+  renderTextBlock(cardElement, '.popup__text--time', createTimeText(data.offer));
   renderAvatarBlock(cardElement, '.popup__avatar', data.author.avatar);
-  renderTextBlock(
-    cardElement,
-    '.popup__text--capacity',
-    numberRoomGuest(data.offer)
-  );
-  renderTextBlock(
-    cardElement,
-    '.popup__text--time',
-    timeText(data.offer)
-  );
-  renderPhotoBlock(cardElement, '.popup__photo', renderPhoto(data.offer.photos));
+  renderPhotoBlock(cardElement, '.popup__photos', data.offer.photos);
+  renderFeaturesBlock(cardElement, '.popup__features', data.offer.features);
 
   return cardElement;
 };
 
 const renderCard = (data) => mapCanvas.append(createCardTemplate(data));
 
-export { renderCard };
+export {renderCard};

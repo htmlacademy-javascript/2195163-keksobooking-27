d0a8a95db56498898c90a0e30b45e6ed4d564a62
TimaRagnar <114170518+TimaRagnar@users.noreply.github.com>
2022-11-03 18:28:13 +0300

    Домашняя работа 8.21

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import {renderCard} from './render-card.js';
 import {createOffers} from './create-offers.js';
-import {switchAdFormState} from './form.js';
+import {deactivateForms, activateForms} from './forms-states.js';
 
 const offer = createOffers()[0];
-console.log(offer);
 renderCard(offer);
-switchAdFormState();
+deactivateForms();
+activateForms();

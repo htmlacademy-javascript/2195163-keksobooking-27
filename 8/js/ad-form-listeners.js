cfc19daec6a26e54208013afa1ee327df7ab33a3
Keks <keks@htmlacademy.ru>
2022-11-05 02:50:21 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/ad-form-listeners.js
+++ 8/js/ad-form-listeners.js
@@ -3,8 +3,8 @@ import {validateForm} from './validate-ad-form.js';
 const adForm = document.querySelector('.ad-form');
 const timeInItem = document.querySelector('#timein');
 const timeoutItem = document.querySelector('#timeout');
-const priceList = document.querySelector('#price');
-const typeList = document.querySelector('#type');
+const priceInput = document.querySelector('#price');
+const typeSelect = document.querySelector('#type');
 
 const minPrice = {
   'bungalow': 0,
@@ -14,29 +14,28 @@ const minPrice = {
   'palace': 10000,
 };
 
-const getTypeChange = () => {
-  priceList.placeholder = minPrice[typeList.value];
-  priceList.min = minPrice[typeList.value];
-  priceList.dataset.pristineMinMessage = `минимальное значение ${minPrice[typeList.value]}`;
+const onTypeSelectChange = () => {
+  priceInput.placeholder = minPrice[typeSelect.value];
+  priceInput.min = minPrice[typeSelect.value];
+  priceInput.dataset.pristineMinMessage = `минимальное значение ${minPrice[typeSelect.value]}`;
 };
-
-typeList.addEventListener('change', getTypeChange);
-priceList.addEventListener('change', getTypeChange);
-
 
 const onTimeInItemChange = () => timeoutItem.value = timeInItem.value;
 const onTimeOutItemChange = () => timeInItem.value = timeoutItem.value;
 
 const onAddFormSubmit = (evt) => {
   evt.preventDefault();
-  if (validateForm()) {
-  }
+  if (validateForm()) {}
 };
+
+/*typeSelect.addEventListener('change', getTypeChange);
+priceItem.addEventListener('change', getTypeChange);*/
 
 const setAdFormListeners = () => {
   adForm.addEventListener('submit', onAddFormSubmit);
   timeInItem.addEventListener('change', onTimeInItemChange);
   timeoutItem.addEventListener('change', onTimeOutItemChange);
+  typeSelect.addEventListener('change', onTypeSelectChange);
 };
 
 export {setAdFormListeners};
